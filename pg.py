0622ebb0130c81953d9a0cae54b3333051932f69
J. Cliff Dyer <cdyer@edx.org>
2016-12-15 11:52:01 -0500

    refactor python demo

--- pg.py
+++ pg.py
@@ -1,31 +1,59 @@
+"""
+Demo postgres connector
+"""
+
+from __future__ import print_function, unicode_literals
+
 import socket
 import struct
+from contextlib import closing
 
+# pylint: disable=invalid-name
 unix_socket = '/run/postgresql/.s.PGSQL.5432'
+unix_socket = '/private/tmp/.s.PGSQL.5432'
+# pylint: enable=invalid-name
 
-sock = socket.socket(socket.AF_UNIX)
-sock.connect(unix_socket)
+def receive(sock):
+    """
+    Get all currently available data from the socket, and return as a single string.
+    """
+    chunk_size = 16
+    chunks = [sock.recv(chunk_size)]
+    while len(chunks[-1]) == chunk_size:
+        chunks.append(sock.recv(chunk_size))
+    return b''.join(chunks)
 
-#                 proto_version params                                        terminator
-startup_message = b'\0\x03\0\0' b'database\0' b'cliff\0' b'user\0' b'cliff\0' b'\0' 
-print(repr(startup_message))
-print(hex(4+ len(startup_message)))
-length = struct.pack('!I', len(startup_message) + 4)
-print([hex(x) for x in length])
-sock.sendall(length)
-sock.sendall(startup_message)
-import time
-print( "Receiving")
-chunks = [sock.recv(16)]
-print(chunks)
-while len(chunks[-1]) == 16:
-    chunks.append(sock.recv(16))
-    print(chunks[-1])
-response = b''.join(chunks)
+def send_startup_message(sock):
+    """
+    Send a startup message
+    """
+    #                 proto_version params                                                terminator
+    startup_message = b'\0\x03\0\0' b'database\0' b'cliffdyer\0' b'user\0' b'cliffdyer\0' b'\0'
+    length = struct.pack('!I', len(startup_message) + 4)
+    startup_message = b''.join([length, startup_message])
+    print(repr(startup_message))
+    sock.sendall(startup_message)
+    return receive(sock)
 
-sock.sendall(b'X\0\0\0\x04')
-res = sock.recv(16)
-print(res)
-sock.close()
-print("done")
-print(response)
+def send_terminate(sock):
+    """
+    End the connection
+    """
+    sock.sendall(b'X\0\0\0\x04')
+    return receive(sock)
+
+def main():
+    """
+    Connect to postgres server.
+    """
+    with closing(socket.socket(socket.AF_UNIX)) as sock:
+        sock.connect(unix_socket)
+        response = send_startup_message(sock)
+        print(response)
+        response = send_terminate(sock)
+        print(response)
+    print("done")
+
+
+if __name__ == '__main__':
+    main()
