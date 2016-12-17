5c97a113bde71a1ee8afa5c790d636f22e4b1552
J. Cliff Dyer <jcd@sdf.org>
2016-12-17 18:02:31 -0500

    Add message parsing to prototype

--- pg.py
+++ pg.py
@@ -10,7 +10,7 @@ from contextlib import closing
 
 # pylint: disable=invalid-name
 unix_socket = '/run/postgresql/.s.PGSQL.5432'
-unix_socket = '/private/tmp/.s.PGSQL.5432'
+# unix_socket = '/private/tmp/.s.PGSQL.5432'
 # pylint: enable=invalid-name
 
 def receive(sock):
@@ -38,7 +38,7 @@ def send_startup_message(sock):
     Send a startup message
     """
     #                 proto_version params                                                terminator
-    startup_message = b'\0\x03\0\0' b'database\0' b'cliffdyer\0' b'user\0' b'cliffdyer\0' b'\0'
+    startup_message = b'\0\x03\0\0' b'database\0' b'cliff\0' b'user\0' b'cliff\0' b'\0'
     startup_message = create_message(None, startup_message)
     print(repr(startup_message))
     sock.sendall(startup_message)
@@ -61,6 +61,23 @@ def send_query(sock):
     sock.sendall(message)
     return receive(sock)
 
+def iter_msgs(inputs):
+    while inputs:
+        length = (inputs[1] << 24) + (inputs[2] << 16) + (inputs[3] << 8) + inputs[4] + 1
+        msg, inputs = inputs[:length], inputs[length:]
+        yield msg
+
+def parse_msg(msg):
+    if not msg:
+        print("No message")
+        return
+    identifier = msg[:1].decode('utf-8')
+    length = (msg[1] << 24) + (msg[2] << 16) + (msg[3] << 8) + msg[4] + 1
+    remainder = msg[5:]
+    print("Message: {}".format(msg))
+    print("  Identifier: {}".format(identifier))
+    print("  Length: {}".format(length))
+    print("  Remainder: {!r}".format(remainder.decode('latin1')))
 
 def main():
     """
@@ -69,11 +86,24 @@ def main():
     with closing(socket.socket(socket.AF_UNIX)) as sock:
         sock.connect(unix_socket)
         response = send_startup_message(sock)
-        print("Startup response", response)
+        print("Startup response")
+        print(response)
+        for msg in iter_msgs(response):
+            parse_msg(msg)
+        print()
+
         response = send_query(sock)
-        print("Query response", response)
+        print("Query response")
+        print(response)
+        for msg in iter_msgs(response):
+            parse_msg(msg)
+        print()
         response = send_terminate(sock)
-        print("Terminate response", response)
+        print("Terminate response")
+        print(response)
+        for msg in iter_msgs(response):
+            parse_msg(msg)
+        print()
     print("done")
 
 
