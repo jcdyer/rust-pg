284648b451cb837fbad6660e5eae437fb3f0661b
J. Cliff Dyer <jcd@sdf.org>
2016-12-17 18:02:14 -0500

    Add realistic input test

--- src/servermsg.rs
+++ src/servermsg.rs
@@ -18,7 +18,9 @@ pub enum ServerMsg<'a> {
     ParamStatus(&'a[u8], &'a[u8]),
     Error(&'a[u8]),
     Notification(&'a[u8]),
-    Row(&'a[u8]),
+    RowDescription(&'a[u8]),
+    RowData(&'a[u8]),
+    Unknown(&'a[u8]),
 }
 
 impl <'a> ServerMsg<'a> {
@@ -32,11 +34,11 @@ impl <'a> ServerMsg<'a> {
             return Err(PgError::Other)
         }
         let identifier = message.get(0).unwrap();
+        let (_, extra) = message.split_at(5);
         if identifier == &_ascii_byte("R") {
             AuthMsg::from_slice(message).map(ServerMsg::Auth)
-        } else if identifier == &_ascii_byte("S") {
-            let (_, param_slice) = message.split_at(5); // strip off the identifier and length
-            let mut param_iter = param_slice.split(|c| c == &0); // split on nulls
+        } else if identifier == &_ascii_byte("S") {  // Parameter Status
+            let mut param_iter = extra.split(|c| c == &0); // split on nulls
             let name = param_iter.next().unwrap();
             let value = param_iter.next().unwrap();
             let nothing = param_iter.next().unwrap(); // The second null is the terminator
@@ -45,6 +47,12 @@ impl <'a> ServerMsg<'a> {
             } else {
                 Ok(ServerMsg::ParamStatus(name, value))
             }
+        } else if identifier == &_ascii_byte("T") {  // Row Description
+            Ok(ServerMsg::RowDescription(extra))  // TBD
+        } else if identifier == &_ascii_byte("D") {  // Data Row
+            Ok(ServerMsg::RowData(extra))  // TBD
+        } else if identifier == &_ascii_byte("Z") {  // ReadyForQuery
+            Ok(ServerMsg::ReadyForQuery)
         } else {
             Err(PgError::Other)
         }
@@ -103,5 +111,40 @@ mod tests {
         let (second, nothing) = take_msg(rest).unwrap();
         assert_eq!(second, [72, 0, 0, 0, 12, 1, 2, 3, 4, 5, 6, 7, 8]);
         assert_eq!(nothing, []);
+    }
+
+    #[test]
+    fn test_server_response_parse() {
+        /// Test a realistic example of a server response.  Need this buffer in bytes, but it's not all utf-8
+
+        let buffer = "R\x00\x00\x00\x08\x00\x00\x00\x00S\x00\x00\x00\x16application_name\x00\x00S\x00\x00\x00\x19client_encoding\x00UTF8\x00S\x00\x00\x00\x17DateStyle\x00ISO, MDY\x00S\x00\x00\x00\x19integer_datetimes\x00on\x00S\x00\x00\x00\x1bIntervalStyle\x00postgres\x00S\x00\x00\x00\x15is_superuser\x00off\x00S\x00\x00\x00\x19server_encoding\x00UTF8\x00S\x00\x00\x00\x19server_version\x009.6.1\x00S\x00\x00\x00 session_authorization\x00cliff\x00S\x00\x00\x00#standard_conforming_strings\x00on\x00S\x00\x00\x00\x18TimeZone\x00US/Eastern\x00K\x00\x00\x00\x0c\x00\x00\x17\u00bb\x15b\u00fb1Z\x00\x00\x00\x05I";
+        let (next, buffer) = take_msg(buffer);
+        let msg = ServerMsg::from_bytes(next).unwrap();
+        assert_eq!(
+            msg,
+            ServerMsg::Auth(AuthMsg::AuthOk),
+        );
+        let (next, buffer) = take_msg(buffer);
+        let msg = ServerMsg::from_bytes(next).unwrap();
+        assert_eq!(
+            msg,
+            ServerMsg::ParamStatus("application_name".as_bytes(), "".as_bytes()),
+        );
+        let (next, buffer) = take_msg(buffer);
+        let msg = ServerMsg::from_bytes(next).unwrap();
+        assert_eq!(
+            msg,
+            ServerMsg::ParamStatus("client_encoding".as_bytes(), "UTF8".as_bytes()),
+        );
+        let (next, buffer) = take_msg(buffer);
+        let msg = ServerMsg::from_bytes(next).unwrap();
+        assert_eq!(
+            msg,
+            ServerMsg::ParamStatus("DateStyle".as_bytes(), "ISO, MDY".as_bytes()),
+        );
+        assert_eq!(
+            buffer.len(),
+            24
+        );
     }
 }
